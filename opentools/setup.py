64d77feae6db7fb21749552c2ac55abd9bfaee63
Bowen Chen <bowen118@hotmail.com>
2025-02-13 17:21:17 -0800

    Update

---
+++ opentools/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opentools',
+    version='0.1.0',
+    # description='A flexible and versatile toolbox agent framework for complex tasks in both general and scientific scenarios.',
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    # author='Pan Lu, Bowen Chen, Sheng Liu',
+    # author_email='lupantech@gmail.com',
+    # url='',  # You can add a GitHub or project URL here
+    packages=find_packages(),
+    # install_requires=open('requirements.txt').read().splitlines(),
+    # classifiers=[
+    #     'Programming Language :: Python :: 3',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent',
+    # ],
+    # python_requires='>=3.10',
+)
