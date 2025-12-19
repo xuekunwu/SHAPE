053dc1c58fc6bfe70c57b4c354091c2e1d241246
xuekun wu <5xuekun@users.noreply.huggingface.co>
2025-12-19 22:50:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
+import sys
 repo_root = os.path.dirname(os.path.abspath(__file__))
 if repo_root not in sys.path:
     sys.path.insert(0, repo_root)
-import sys
 import json
 import argparse
 import time
